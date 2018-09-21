2b256bc29473c0ed8650d191cc48d4eeec9a9797
Kevin Olinger <kolinger@globalpensionassociates.com>
2018-09-21 09:39:01 -0700

    Initial Commit

---
+++ StreamingSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamingSampleApp
+//
+//  Created by Kevin Olinger on 9/21/18.
+//  Copyright © 2018 Universal Music Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
